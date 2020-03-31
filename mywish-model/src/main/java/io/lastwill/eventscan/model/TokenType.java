4df55d5fa3811b6a1fdf7995fba66fcdfa614add
Mikhail R <mrozdin@yandex.ru>
2020-03-31 13:52:44 +0300

    rename token type

--- mywish-model/src/main/java/io/lastwill/eventscan/model/TokenType.java
+++ mywish-model/src/main/java/io/lastwill/eventscan/model/TokenType.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum  TokenType {
-    SMALL("8GRAMM"),
-    BIG("10GRAMM");
+    SMALL("8GRAM"),
+    BIG("10GRAM");
 
     private String name;
     TokenType(String name) {this.name = name;}
